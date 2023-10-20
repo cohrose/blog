3ad16bf8b37d2093c84092027561ce9b68011cf0
Courtney <cohrose@outlook.com>
2023-10-20 17:39:33 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <h1>Welcome to my Blog!</h1>
+    <p>
+      Hi! I primarily work on front end coding, so that's what I'll mainly be writing about. I try to keep my notes
+      short and concise, but still include all relavant info.
+    </p>
+  </header>
+  <!-- <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  padding: 15px;
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,62 @@
+<script setup>
+let posts = [
+  { title: 'the title here', date: 'Oct 20, 2023', tag: 'Angular' },
+  { title: 'really cool stuff', date: 'Oct 20, 2023', tag: 'Something' },
+  { title: 'more things here', date: 'Oct 20, 2023', tag: 'Code here' },
+  { title: 'the title here', date: 'Oct 20, 2023', tag: 'Angular' },
+  { title: 'really cool stuff', date: 'Oct 20, 2023', tag: 'Something' },
+  { title: 'more things here', date: 'Oct 20, 2023', tag: 'Code here' },
+];
+</script>
+
+<template>
+  <div>
+    <div class="blog">
+      <div class="post" v-for="(item, i) in posts" :key="i">
+        <div class="title">{{ item.title }}</div>
+        <div class="inner-grid">
+          <div class="date">{{ item.date }}</div>
+          <div class="tag">
+            <span>{{ item.tag }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.blog {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 15px;
+}
+
+.post {
+  padding: 15px;
+  background: var(--dark-gray);
+  border-radius: 10px;
+}
+
+.post .title {
+  color: var(--cyan);
+  font-size: 18px;
+  margin-bottom: 8px;
+}
+
+.inner-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.post .tag {
+  text-align: end;
+}
+
+.tag span {
+  background: var(--cyan);
+  color: var(--dark-gray);
+  padding: 2px 4px;
+  border-radius: 5px;
+  font-weight: 600;
+}
+</style>
