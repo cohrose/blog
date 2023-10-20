ca61c7576cf2471111a07c8096d9dfde28cc6fd4
Courtney <cohrose@outlook.com>
2023-10-20 18:30:37 -0400

    remove base url

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   build: {
     outDir: 'docs',
   },
-  base: 'https://courtneymedellin.com/blog/', // for subdomains use full custom url
+  //base: 'https://courtneymedellin.com/blog/', // for subdomains use full custom url
   assetsInclude: '**/*.md',
   publicPath: '/blog/',
 });
