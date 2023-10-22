83fa0b947e30f482dd7092e1cf01c0fe73887daa
Courtney <cohrose@outlook.com>
2023-10-22 14:52:25 -0400

    update custom email post

--- src/components/posts/custom-email.vue
+++ src/components/posts/custom-email.vue
@@ -3,29 +3,29 @@ import { marked } from 'marked';
 const code = `# Custom Email Domain
 
 ## Set Up Custom Domain
-1. Purchase a custom domain name; I used ~[namecheap](https://www.namecheap.com/)~.
-2. Purchase an ~[email account](https://www.namecheap.com/hosting/email/)~ under the domain provider.
+1. Purchase a custom domain name; I used [Namecheap](https://www.namecheap.com/).
+2. Purchase an [email account](https://www.namecheap.com/hosting/email/) under the domain provider.
 3. Create a mailbox for your custom domain.
    * Choose a mailbox name, example: \`john@yourdomain.com\`
    * Create a password for your new mailbox. You will need this later on.
 4. Go to the DNS settings for your domain, and make sure “Private Email” is selected for your Mail Settings.
 
 ## Add Email to Email client
-1. Choose email client, I chose ~[Spark](https://sparkmailapp.com/)~.
+1. Choose email client, I chose [Spark](https://sparkmailapp.com/).
 2. Type in the email address, and password you created from earlier.
 3. Choose “Additional Settings”
 4. Here you will configure the custom domain settings:
 
 ### Inbox server (IMAP)
-* **Username**: your full email address (ex. ~[john@yourdomain.com](mailto:john@yourdomain.com)~)
+* **Username**: your full email address (ex. [john@yourdomain.com](mailto:john@yourdomain.com))
 * **Password**: password for this mail account
-* **Server**: ~[mail.privateemail.com](http://mail.privateemail.com/)~
+* **Server**: mail.privateemail.com
 * **Port**: 993
 * **Protection**: SSL
 ### Outbox server (SMTP)
-* **Username**: your full email address (ex. ~[john@yourdomain.com](mailto:john@yourdomain.com)~)
+* **Username**: your full email address (ex. [john@yourdomain.com](mailto:john@yourdomain.com))
 * **Password**: password for this mail account
-* **Server**: ~[mail.privateemail.com](http://mail.privateemail.com/)~
+* **Server**: mail.privateemail.com
 * **Port**: 465
 * **Protection**: SSL
 
